789e2bbe954df52a9e9508dde2983c4ec6396d0c
Jason Brown <jason.brown.delta@gmail.com>
2015-06-17 12:58:51 +0100

    removed state listener for ionic view

--- www/js/controllers/article.js
+++ www/js/controllers/article.js
@@ -2,14 +2,4 @@ angular.module('ionPress')
     .controller('ArticleCtrl', function ($rootScope, $stateParams, $scope, articleService) {
         $scope.articles = $stateParams.articles;
         $scope.article  = articleService.getArticleById($stateParams.id);
-
-        $scope.hideBar = false;
-
-        $rootScope.$on('$ionicView.beforeEnter', function (event, view) {
-            if(view.stateName === 'app.article') {
-                $scope.hideBar = true;
-            }else{
-                $scope.hideBar = false;
-            }
-        });
     });
